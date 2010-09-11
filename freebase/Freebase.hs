d42c6f0634f2eabf3a34d03d070c446345a09ab0
roberts <robertsevern@gmail.com>
2010-09-11 15:20:29 +0100

    getting a good list of directors

--- freebase/Freebase.hs
+++ freebase/Freebase.hs
@@ -2,9 +2,11 @@ module Freebase where
 
 import System.Random
 import Text.JSON
+import Text.JSON.Types
 import Network.HTTP
 import Network.URI
-
+import Data.List (sortBy)
+import Data.Ord (comparing)
 import Control.Monad
 
 import Data.Maybe (fromJust)
@@ -46,7 +48,34 @@ runSimpleQuery qtype key name = do
       l = lookupValue (getFirst k) key
       m = fmap (\(JSArray x) -> x) l
   return $ fmap (map (\(JSString x) -> fromJSString x)) m
+  
 
+getDirectorBigBudgetFilms :: IO (Result [(String, Int)])
+getDirectorBigBudgetFilms = do
+  let budgetQueryObject = showJSON (toJSObject [("amount", JSNull), ("currency", showJSON "US$")])
+      filmQueryObject = showJSON (toJSObject [("name", JSNull), ("limit", showJSON (5 :: Int)), ("sort", showJSON "-estimated_budget.amount"), ("estimated_budget", budgetQueryObject)]);
+  response <- makeQuery $ mkSimpleQuery [("type",showJSON "/film/director"),("id",showJSON JSNull), ("limit",showJSON (600 :: Int)),("film", JSArray [filmQueryObject])]
+  let arrayOfDirAndFilms = (lookupValue response "result" :: Result JSValue)
+  return (fmap extractDirAndBudgets arrayOfDirAndFilms)
+  
+extractDirAndBudgets :: JSValue -> [(String,Int)]
+extractDirAndBudgets (JSArray xs) = sortBy (\(_,a) (_,b) -> compare b a) $ map extractDirAndBudget xs
+extractDirAndBudgets _ = error "Freebase screwed us."
+  
+extractDirAndBudget :: JSValue -> (String,Int)
+extractDirAndBudget (JSObject s) = (idDir, extractBudget $ fromJust $ get_field s "film")
+	where
+		idDir = (\(JSString z) -> fromJSString z) (fromJust $ get_field s "id")
+extractDirAndBudget _ = undefined
+
+extractBudget :: JSValue -> Int
+extractBudget (JSArray films) = sum $ map getFilmBudget films
+
+getFilmBudget :: JSValue -> Int
+getFilmBudget (JSObject film) = truncate cost 
+	where
+		(JSObject estimatedBudget) = fromJust $ get_field film "estimated_budget"
+		(JSRational _ cost) = fromJust $ get_field estimatedBudget "amount"
 getAlbumList :: String -> IO (Result [String])
 getAlbumList = runSimpleQuery "/music/artist" "album" 
 
