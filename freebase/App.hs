6735e78d352e4b418048ecf3582699874853e5b8
Jeff Foster <jeff.foster@acm.org>
2010-09-21 21:27:28 +0100

    add IO

---
+++ freebase/App.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE TypeFamilies, QuasiQuotes, TemplateHaskell #-}
+import Yesod
+import Yesod.Helpers.Static
+
+import Freebase
+import Films
+import Logic
+
+import Text.JSON
+
+-- Site specific data is stored here
+data QuizMaster = QuizMaster {
+      ajaxStatic :: Static
+}
+
+staticFiles "static/"
+
+mkYesod "QuizMaster" [$parseRoutes|                      
+  /       HomeR   GET
+  /static StaticR Static ajaxStatic
+|]
+
+instance Yesod QuizMaster where
+    approot _ = ""
+   
+getHomeR :: GHandler sub QuizMaster RepHtml
+getHomeR = hamletToRepHtml [$hamlet|
+%html
+  %head
+    %title Quiz Master
+    %link!rel="stylesheet"!href=@StaticR.albums_css@
+    %script!src="http://code.jquery.com/jquery-1.4.2.min.js"
+    %script!src=@StaticR.script_js@
+  %body
+    %h1 AwesomeQuiz.com
+    %p AwesomeQuiz provides a wide ranging set of questions to tax your brain.
+    %hr
+    %p Written using 
+        %a!href="http://docs.yesodweb.com/Yesod" Yesod Web Framework
+|]              
+
+main :: IO ()
+main = do
+  let static = fileLookupDir "static/" typeByExt
+  basicHandler 3000 $ QuizMaster static
