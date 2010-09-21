720299984604e6301b5bb7c96a242ac7a3800267
Jeff Foster <jeff.foster@acm.org>
2010-09-21 19:54:28 +0100

    begin the question making process.

--- freebase/Logic.hs
+++ freebase/Logic.hs
@@ -5,4 +5,11 @@ data QuestionFormat = MultipleChoice [String] String
                     | FreeText String
                     | MultipleFreeText [String]
 
+-- |A question is a question format, together with a description
+data Question = Question Description QuestionFormat
+
+-- |A question maker uses some logic to generate questions
+class QuestionMaker a where
+    generateQuestion :: a -> Question
+
     
