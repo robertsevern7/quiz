a7896f1e6f627dfa5e8ed9a53b6ad234dd99588b
Jeff Foster <jeff.foster@acm.org>
2010-09-21 20:24:02 +0100

    declaration of showJSON for question, still need to do QuestionFormat

--- freebase/Logic.hs
+++ freebase/Logic.hs
@@ -7,6 +7,12 @@ data QuestionFormat = MultipleChoice [String] String
                     | FreeText String
                     | MultipleFreeText [String]
 
+instance JSON QuestionFormat where
+    readJSON jsValue = undefined
+    showJSON (MultipleChoice choices answer) = undefined
+    showJSON (FreeText answer) = undefined
+    showJSON (MultipleFreeText answers) = undefined
+
 -- |May want to change this to something "formattable"
 type Description = String
 
@@ -16,7 +22,8 @@ data Question = Question Description QuestionFormat
 -- |When a Question is 
 instance JSON Question where
     readJSON jsValue = undefined
-    showJSON (Question description questionFormat) = undefined
+    showJSON (Question description questionFormat) = makeObj [("question", JSString $ toJSString description)
+                                                             ,("format", showJSON questionFormat)]
 
 -- |A question maker uses some logic to generate questions
 class QuestionMaker a where
