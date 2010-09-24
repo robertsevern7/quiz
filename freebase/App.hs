f617966910b8f0421a72b08a66124ac183bd25d7
Jeff Foster <jeff.foster@acm.org>
2010-09-24 21:11:35 +0100

    start sketching out the two pages

--- freebase/App.hs
+++ freebase/App.hs
@@ -18,10 +18,18 @@ staticFiles "static/"
 mkYesod "QuizMaster" [$parseRoutes|                      
   /       HomeR   GET
   /static StaticR Static ajaxStatic
+  /actors ActorsR GET
+  /films  FilmsR GET
 |]
 
 instance Yesod QuizMaster where
     approot _ = ""
+
+getActorsR :: GHandler sub QuizMaster RepHtml
+getActorsR = undefined
+
+getFilmsR :: GHandler sub QuizMaster RepHtml
+getFilmsR = undefined
    
 getHomeR :: GHandler sub QuizMaster RepHtml
 getHomeR = hamletToRepHtml [$hamlet|
@@ -33,7 +41,10 @@ getHomeR = hamletToRepHtml [$hamlet|
     %script!src=@StaticR.script_js@
   %body
     %h1 AwesomeQuiz.com
-    %p AwesomeQuiz provides a wide ranging set of questions to tax your brain.
+    %p AwesomeQuiz provides a wide ranging set of questions to tax your brain.  Pick from one of the following categories to get a randomly selected question.
+    %ul
+      %li Film Directors
+      %li Film Actors
     %hr
     %p Written using 
         %a!href="http://docs.yesodweb.com/Yesod" Yesod Web Framework
