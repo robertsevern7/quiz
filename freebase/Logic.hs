349d1e263f8547009deb03ee27e42c58d76f05d5
Jeff Foster <jeff.foster@acm.org>
2010-09-24 21:21:30 +0100

    Fix some the the million and one warnings!

--- freebase/Logic.hs
+++ freebase/Logic.hs
@@ -16,7 +16,7 @@ mkString = JSString . toJSString
 -- TODO hideous duplication here
 -- TODO nicer way of expressing the answer would be good
 instance JSON QuestionFormat where
-    readJSON jsValue = Error "No need to support this just yet"
+    readJSON _ = Error "No need to support this just yet"
     showJSON (MultipleChoice choices answer) = makeObj [("questionType", mkString "multipleChoice")
                                                        ,("choices", JSArray (map mkString choices))
                                                        ,("answer", mkString answer)]
@@ -36,7 +36,7 @@ data Question = Question Description QuestionFormat deriving Show
 
 -- |When a Question is 
 instance JSON Question where
-    readJSON jsValue = Error "No need to support this just yet"
+    readJSON _ = Error "No need to support this just yet"
     showJSON (Question description questionFormat) = makeObj [("question", mkString description)
                                                              ,("format", showJSON questionFormat)]
 
