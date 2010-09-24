6c90aaddd1479468f12c06f0e89868807c89c627
Jeff Foster <jeff.foster@acm.org>
2010-09-24 21:55:33 +0100

    Keep going, eventually I'll be able to render something...

--- freebase/App.hs
+++ freebase/App.hs
@@ -30,8 +30,15 @@ questionTemplate (Question description (IdentifyFrom choices answer)) = identify
 questionTemplate (Question description _) = error "This has not been implemented yet."
 
 identifyFromTemplate :: Description -> [String] -> String -> Hamlet (Route QuizMaster)
-identifyFromTemplate description choices answer = undefined
-
+identifyFromTemplate description choices answer = undefined [$hamlet|
+  %html
+    %head
+      %title $description$
+    %body
+      %h1 $description$
+      %p And some rendering of said question here.
+  |]
+                                                   
 getActorsR :: Handler RepHtml
 getActorsR = undefined
 
@@ -40,22 +47,23 @@ getFilmsR = undefined
    
 getHomeR :: Handler RepHtml
 getHomeR = hamletToRepHtml [$hamlet|
-%html
-  %head
-    %title Quiz Master
-    %link!rel="stylesheet"!href=@StaticR.albums_css@
-    %script!src="http://code.jquery.com/jquery-1.4.2.min.js"
-    %script!src=@StaticR.script_js@
-  %body
-    %h1 AwesomeQuiz.com
-    %p AwesomeQuiz provides a wide ranging set of questions to tax your brain.  Pick from one of the following categories to get a randomly selected question.  Yes, it's crap at the moment, but that's because it's a beta.
-    %ul
-      %li Film Directors
-      %li Film Actors
-    %hr
-    %p Written using 
-        %a!href="http://docs.yesodweb.com/Yesod" Yesod Web Framework
-|]              
+  !!!
+  %html
+    %head
+      %title Quiz Master
+      %link!rel="stylesheet"!href=@StaticR.albums_css@
+      %script!src="http://code.jquery.com/jquery-1.4.2.min.js"
+      %script!src=@StaticR.script_js@
+    %body
+      %h1 AwesomeQuiz.com
+      %p AwesomeQuiz provides a wide ranging set of questions to tax your brain.  Pick from one of the following categories to get a randomly selected question.  Yes, it's crap at the moment, but that's because it's a beta.
+      %ul
+        %li Film Directors
+        %li Film Actors
+      %hr
+      %p Written using 
+          %a!href="http://docs.yesodweb.com/Yesod" Yesod Web Framework
+  |]              
 
 main :: IO ()
 main = do
