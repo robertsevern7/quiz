01bb7072ce17cd1a62b3e74fecc4f852a8231414
Jeff Foster <jeff.foster@acm.org>
2010-09-28 21:20:18 +0100

    use new thing everywhere.

--- freebase/JsonHelper.hs
+++ freebase/JsonHelper.hs
@@ -3,6 +3,8 @@ module JsonHelper (
                   ,getFirst
                   ,getString
                   ,getJSValue
+                  ,mkPath
+                  ,mkIndex
                   ) where
 
 import Text.JSON
@@ -11,6 +13,12 @@ import Data.List ((!!))
 
 data PathSegment = NamedField String
                  | ArrayIndex Int
+
+mkPath :: String -> PathSegment
+mkPath = NamedField 
+
+mkIndex :: Int -> PathSegment
+mkIndex = ArrayIndex
 
 -- Should this use fmap?
 lookupValue :: JSON a => Result JSValue -> String -> Result a
@@ -22,21 +30,23 @@ getFirst (Ok (JSArray (x:xs))) = Ok x
 getFirst _ = Error "Not an array"
 
 -- Get JS values by following a path indexing into fields as required
+{-
 getJSValue :: JSValue -> [String] -> Maybe JSValue
 getJSValue j p = getJSValue' (Just j) p
     where
       getJSValue' Nothing _                    = Nothing
       getJSValue' (Just jsvalue) []            = Just jsvalue
-      getJSValue' (Just (JSObject obj)) (x:xs) = getJSValue' (get_field obj x) xs
+      getJSValue' (Just (JSObject obj)) (x:xs) = getJSValue' (get_field obj x) xs -}
 
-getJSValue2 :: JSValue -> [PathSegment] -> Maybe JSValue
-getJSValue2 j p = getJSValue2' (Just j) p
+getJSValue :: JSValue -> [PathSegment] -> Maybe JSValue
+getJSValue j p = getJSValue' (Just j) p
     where
-      getJSValue2' Nothing _ = Nothing
-      getJSValue2' (Just jsValue) [] = Just jsValue
-      getJSValue2' (Just (JSArray array)) ((ArrayIndex x):xs) | x < (length array - 1) = getJSValue2' (Just (array !! x)) xs
-                                                              | otherwise = getJSValue2' Nothing xs
-      getJSValue2' (Just (JSObject obj)) ((NamedField x):xs) = getJSValue2' (get_field obj x) xs
+      getJSValue' Nothing _ = Nothing
+      getJSValue' (Just jsValue) [] = Just jsValue
+      getJSValue' (Just (JSArray array)) ((ArrayIndex x):xs) | x < (length array - 1) = getJSValue' (Just (array !! x)) xs
+                                                             | otherwise = getJSValue' Nothing xs
+      getJSValue' (Just (JSObject obj)) ((NamedField x):xs) = getJSValue' (get_field obj x) xs
+      getJSValue' _ _ = Nothing
 
 -- Get the string value or error
 getString :: JSValue -> String
