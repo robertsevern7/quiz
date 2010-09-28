bff842f2f7e07f89253d4c3ad9ebee640af31161
Jeff Foster <jeff.foster@acm.org>
2010-09-28 21:00:43 +0100

    factor off helper functions

--- freebase/JsonHelper.hs
+++ freebase/JsonHelper.hs
@@ -2,9 +2,11 @@ module JsonHelper (
                    lookupValue
                   ,getFirst
                   ,getString
+                  ,getJSValue
                   ) where
 
 import Text.JSON
+import Text.JSON.Types
 
 -- Should this use fmap?
 lookupValue :: JSON a => Result JSValue -> String -> Result a
@@ -15,6 +17,15 @@ getFirst :: Result JSValue -> Result JSValue
 getFirst (Ok (JSArray (x:xs))) = Ok x
 getFirst _ = Error "Not an array"
 
-getString :: Result JSValue -> String
-getString (Ok (JSString x)) = fromJSString x
-getString _ = error "No string found when expected."
+{- Boring helper functions -}
+-- Get JS values by following a path indexing into fields as required
+getJSValue :: JSValue -> [String] -> Maybe JSValue
+getJSValue j p = getJSValue' (Just j) p
+    where
+      getJSValue' Nothing _                    = Nothing
+      getJSValue' (Just jsvalue) []            = Just jsvalue
+      getJSValue' (Just (JSObject obj)) (x:xs) = getJSValue' (get_field obj x) xs
+
+getString :: JSValue -> String
+getString (JSString x) = fromJSString x
+getString x = error $ "No string found when expected. =" ++ show x
