98f7c025e2dc2b4fec106a992b1c94130f921c3a
Jeff Foster <jeff.foster@acm.org>
2010-10-23 22:50:19 +0100

    redundant bracket

--- freebase/Films.hs
+++ freebase/Films.hs
@@ -77,7 +77,7 @@ rndSelect :: [a] -> Int -> IO [a]
 rndSelect xs n 
     | n < 0     = error "N must be greater than zero."
     | otherwise = replicateM n rand
-        where rand = do r <- randomRIO (0, (length xs) - 1)
+        where rand = do r <- randomRIO (0, length xs - 1)
                         return (xs!!r)
 						
 getTaglineFilmPairs :: JSValue -> [(String, String)]						
