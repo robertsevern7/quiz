88a2cad2329bdc2d2dfeb69d84aae4a996e59341
Jeff Foster <jeff.foster@acm.org>
2010-10-30 22:35:55 +0100

    Add a plugin to shuffle elements about randomly.  This'll be useful for the drag/drop get the answers thing I'm currently thinking about writing.

--- freebase/static/script.js
+++ freebase/static/script.js
@@ -1,3 +1,22 @@
+// A Jquery plugin to do shuffling.
+// Available under MIT license, so just nicked and placed here
+// Original source: http://yelotofu.com/labs/jquery/snippets/shuffle/jquery.shuffle.js
+(function($){
+
+	$.fn.shuffle = function() {
+		return this.each(function(){
+			var items = $(this).children().clone(true);
+			return (items.length) ? $(this).html($.shuffle(items)) : this;
+		});
+	}
+	
+	$.shuffle = function(arr) {
+		for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+		return arr;
+	}
+	
+})(jQuery);
+
 $(document).ready(function() {
 	$('#identifyAnswer').val("");
     $('#revealAnswers,#nextQuestion').button();
