779abd4ecf5afa4d57b577e657ce0ecdf7e9d1cd
Jeff Foster <jeff.foster@acm.org>
2010-11-09 21:59:09 +0000

    move document on ready appropriately

--- freebase/static/script.js
+++ freebase/static/script.js
@@ -17,41 +17,6 @@
 	
 })(jQuery);
 
-$(document).ready(function() {
-	$('#identifyAnswer').val("");
-    $('#revealAnswers,#nextQuestion').button();
-    
-    function showNextButton() {
-        $('#nextQuestion').removeClass('hiddenButton');
-        $('#nextQuestion').addClass('visibleButton');
-    }
-    
-    function showAnswer(index, answer) {
-        $(answer).fadeIn('slow', function() {
-            $(answer).removeClass('hiddenAnswer');
-            $(answer).addClass('revealedAnswer');
-            
-            if (!$('.hiddenAnswer').size()) {
-                showNextButton();
-            }
-        });
-    }
-	
-	$('#identifyAnswer').keyup(function() {
-		$('.hiddenAnswer').each(function(index, answer) {
-			if (sanitise($('#identifyAnswer').val()) === sanitise($(this).text())) {
-				showAnswer(index, answer);
-				$('#identifyAnswer').val("");
-			}
-		});
-	});
-    
-    $('#revealAnswers').click(function() {
-        $('.hiddenAnswer').each(showAnswer);
-        showNextButton()
-    })
-});
-
 function sanitise(input) {
 	return input.toLowerCase().replace(/[^0-9A-Za-z]/g, "");
 }
--- freebase/static/scripts/identifyFrom.js
+++ freebase/static/scripts/identifyFrom.js
@@ -1,3 +1,34 @@
 $(document).ready(function() {
-        // TODO Put specific identifyFrom template information in here.
+	$('#identifyAnswer').val("");
+    $('#revealAnswers,#nextQuestion').button();
+    
+    function showNextButton() {
+        $('#nextQuestion').removeClass('hiddenButton');
+        $('#nextQuestion').addClass('visibleButton');
+    }
+    
+    function showAnswer(index, answer) {
+        $(answer).fadeIn('slow', function() {
+            $(answer).removeClass('hiddenAnswer');
+            $(answer).addClass('revealedAnswer');
+            
+            if (!$('.hiddenAnswer').size()) {
+                showNextButton();
+            }
+        });
+    }
+	
+	$('#identifyAnswer').keyup(function() {
+		$('.hiddenAnswer').each(function(index, answer) {
+			if (sanitise($('#identifyAnswer').val()) === sanitise($(this).text())) {
+				showAnswer(index, answer);
+				$('#identifyAnswer').val("");
+			}
+		});
+	});
+    
+    $('#revealAnswers').click(function() {
+        $('.hiddenAnswer').each(showAnswer);
+        showNextButton()
+    })
 });
