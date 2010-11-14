3c95b425beed2f48da2e79147979738e31cbbfac
Jeff Foster <jeff.foster@acm.org>
2010-11-14 23:07:48 +0000

    get the type signatures right

--- bustaquiz/Handler/Capital.hs
+++ bustaquiz/Handler/Capital.hs
@@ -2,13 +2,16 @@
 module Handler.Capital where
 
 import Quiz
-import Handler.Generic (runQuestion)
+import Handler.Generic (runQuestion,questionWidget)
+import Exception(message,internal)
 
 getCapitalsR :: Int -> Handler RepHtml
 getCapitalsR seed = do
   quiz <- getYesod
-  question <- liftIO $ runQuestion seed (whichCapital quiz)
-  defaultLayout $ do
-    setTitle "Give the capital cities as quick as you can."
-  
+  generatedQuestion <- liftIO $ runQuestion seed (whichCapital quiz)
+  case generatedQuestion of
+    (Left ex) -> invalidArgs ["Failed to generate valid question.", message ex, internal ex]
+    (Right question) -> defaultLayout $ do
+      setTitle "Give the capital cities as quick as you can."
+      addWidget (questionWidget (CapitalsR (succ seed)) question)
 
