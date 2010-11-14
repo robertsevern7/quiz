559168c8d346ad44aafce3ba410c8d92cd0c7930
Jeff Foster <jeff.foster@acm.org>
2010-11-14 23:07:48 +0000

    get the type signatures right

--- bustaquiz/Handler/Generic.hs
+++ bustaquiz/Handler/Generic.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE QuasiQuotes, OverloadedStrings, TemplateHaskell #-}
 module Handler.Generic (
-  runQuestion) where
+  runQuestion,
+  questionWidget) where
 
 import Control.Exception (try,evaluate)
 import Logic
@@ -12,8 +13,8 @@ runQuestion :: QuestionMaker a => Int -> a -> IO (Either QuizException Question)
 runQuestion seed qm = try (evaluate =<< generateQuestion seed qm)
                                              
 -- Display the question as a widget
-questionTemplate :: QuizRoute -> Question -> Widget ()
-questionTemplate route (Question description (Associate pairs)) = undefined  -- $(hamletFile "associate")
-questionTemplate _ (Question _ _) = error "This has not been implemented yet."
+questionWidget :: QuizRoute -> Question -> Widget ()
+questionWidget route (Question description (Associate pairs)) = undefined  -- $(hamletFile "associate")
+questionWidget _ (Question _ _) = error "This has not been implemented yet."
 
 
