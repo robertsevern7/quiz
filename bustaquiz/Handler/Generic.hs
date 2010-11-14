f6dcc3bbf5ba78f210a4fd061b0fae5ac3b880e8
Jeff Foster <jeff.foster@acm.org>
2010-11-14 23:14:36 +0000

    update to remove home page javascript

--- bustaquiz/Handler/Generic.hs
+++ bustaquiz/Handler/Generic.hs
@@ -8,13 +8,15 @@ import Logic
 import Exception
 import Quiz (Widget,QuizRoute)
 import Settings (hamletFile)
+import Yesod.Widget (addHamlet)
 
 runQuestion :: QuestionMaker a => Int -> a -> IO (Either QuizException Question)
 runQuestion seed qm = try (evaluate =<< generateQuestion seed qm)
                                              
 -- Display the question as a widget
 questionWidget :: QuizRoute -> Question -> Widget ()
-questionWidget route (Question description (Associate pairs)) = undefined  -- $(hamletFile "associate")
+questionWidget route (Question description (Associate pairs)) = do
+  addHamlet $(hamletFile "associate")
 questionWidget _ (Question _ _) = error "This has not been implemented yet."
 
 
