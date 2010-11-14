b0695bfcd32174ef5bda7c8b0ed17c84a6681bea
Jeff Foster <jeff.foster@acm.org>
2010-11-14 22:52:22 +0000

    I don't think the generic way we were doing it before was actually buying us anything.

--- bustaquiz/Handler/Capital.hs
+++ bustaquiz/Handler/Capital.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE TemplateHaskell, OverloadedStrings #-}
 module Handler.Capital where
 
-import Control.Exception (try,evaluate)
-import Logic
-import Exception
-
 import Quiz
-import Handler.Generic
+import Handler.Generic (runQuestion)
 
 getCapitalsR :: Int -> Handler RepHtml
-getCapitalsR seed = undefined
+getCapitalsR seed = do
+  quiz <- getYesod
+  question <- liftIO $ runQuestion seed (whichCapital quiz)
+  defaultLayout $ do
+    setTitle "Give the capital cities as quick as you can."
+  
 
--- bustaquiz/Handler/Generic.hs
+++ bustaquiz/Handler/Generic.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE QuasiQuotes, OverloadedStrings, TemplateHaskell #-}
 module Handler.Generic (
-  runQuestion,
-  getQuestionSource)  where
+  runQuestion) where
 
 import Control.Exception (try,evaluate)
 import Logic
@@ -12,18 +11,4 @@ import Settings (hamletFile)
 runQuestion :: QuestionMaker a => Int -> a -> IO (Either QuizException Question)
 runQuestion seed qm = try (evaluate =<< generateQuestion seed qm)
                                              
-getQuestionSource :: QuestionMaker a => (Quiz -> a) -> Int -> QuizRoute -> Handler RepHtml
-getQuestionSource getQuestion seed route = do
-  quizMaster <- getYesod
-  generatedQuestion <- liftIO $ runQuestion seed (getQuestion quizMaster)
-  case generatedQuestion of
-    (Left ex)        -> invalidArgs ["Failed to generate valid question.", message ex, internal ex]
-    (Right question) -> undefined
-    
-    {-
-      let body = (questionTemplate route question)
-          additionalJS = (jsSource question)
-          questions = $(hamletFileDebug "templates/bodybase.hamlet")
-      defaultLayout $ do
-        addHamletHead (headTemplate additionalJS)
-        addHamlet  questions-}
+
