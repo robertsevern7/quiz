2337a1ad96b990865c990732d974d725e98fe629
Jeff Foster <jeff.foster@acm.org>
2010-11-16 13:33:10 +0000

    start wrapping.

--- bustaquiz/Freebase.hs
+++ bustaquiz/Freebase.hs
@@ -15,6 +15,8 @@ import Data.Object
 import Data.Object.Json
 import qualified Data.ByteString.Char8 as B
 
+import Debug.Trace
+
 touch :: String
 touch = "http://api.freebase.com/api/service/touch" 
 
@@ -34,17 +36,18 @@ mqlReadUri = "http://api.freebase.com/api/service/mqlread"
 
 runQuery :: JsonObject -> IO JsonObject
 runQuery s = do
-  a <- simpleHTTP (getRequest (mqlReadUri ++ "?query=" ++ (urlEncode . B.unpack) (encode s))) >>= getResponseBody
+  let request = (getRequest (mqlReadUri ++ "?query=" ++ (urlEncode . B.unpack) (encode s)))
+  a <- simpleHTTP request >>= getResponseBody
   decode (B.pack a)
 
 mkSimpleQuery :: [(String,JsonScalar)] -> JsonObject
-mkSimpleQuery x = toJsonObject $ Mapping (map (\(a,b) -> (B.pack a,Scalar b)) x)
+mkSimpleQuery x = toJsonObject $ Mapping [(B.pack "query",toJsonObject $ Mapping (map (\(a,b) -> (B.pack a,Scalar b)) x))]
 
-
-{-
-runSimpleQuery :: String -> String -> String -> JSValue -> (JSValue -> String) -> IO (String, Result [String])
+-- runSimpleQuery :: String -> String -> String -> JsonScalar -> (JsonScalar -> String) -> IO (String, Result [String])
 runSimpleQuery qtype key id_ arr extractor = do
-  response <- runQuery $ mkSimpleQuery [("type",showJSON qtype),("id",showJSON id_), ("name", JSNull),(key,arr)]
+  response <- runQuery $ mkSimpleQuery [("type",toJsonScalar qtype),("id",toJsonScalar id_), ("name", JsonNull),(key,arr)]
+  return response
+  {-
   let k = lookupValue response "result"
       (Ok name) = lookupValue (getFirst k) "name"
       l = lookupValue (getFirst k) key
