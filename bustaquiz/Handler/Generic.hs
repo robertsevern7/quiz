ded4bb7705e42a75aedd24483469055f9ed39306
Jeff Foster <jeff.foster@acm.org>
2010-11-16 10:55:48 +0000

    link in style sheet.  Deliberately looks naff

--- bustaquiz/Handler/Generic.hs
+++ bustaquiz/Handler/Generic.hs
@@ -7,8 +7,8 @@ import Control.Exception (try,evaluate)
 import Logic
 import Exception
 import Quiz (Widget,QuizRoute,StaticRoute)
-import Settings (hamletFile,juliusFile,jqueryURL)
-import Yesod.Widget (addHamlet,addJulius,addScriptRemote)
+import Settings (cassiusFile,hamletFile,juliusFile,jqueryURL)
+import Yesod.Widget (addCassius,addHamlet,addJulius,addScriptRemote)
 import Yesod.Helpers.Static
 import StaticFiles
 import Data.Either
@@ -19,11 +19,16 @@ runQuestion seed qm = try (evaluate =<< generateQuestion seed qm)
 -- Display the question as a widget
 questionWidget :: QuizRoute -> Question -> Widget ()
 questionWidget route (Question description (Associate pairs)) = do
-  addHamlet $(hamletFile "associate")
-  addScriptRemote jqueryURL -- TODO use addScriptEither and get local copy
+  -- External requirements
+  addScriptRemote jqueryURL
   addJulius $(juliusFile "shuffle")
   addJulius $(juliusFile "text")
+  
+  -- Actual code
+  addHamlet $(hamletFile "associate")
+  addCassius $(cassiusFile "associate")
   addJulius $(juliusFile "associate")
+  
 questionWidget _ (Question _ _) = error "This has not been implemented yet."
 
 
