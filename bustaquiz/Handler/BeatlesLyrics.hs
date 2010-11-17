08c73454e9ad0337ef588de3cf3a49e9b56175e7
Jeff Foster <jeff.foster@acm.org>
2010-11-17 13:59:13 +0000

    Drastically simplify the rooting.

--- bustaquiz/Handler/BeatlesLyrics.hs
+++ bustaquiz/Handler/BeatlesLyrics.hs
@@ -2,16 +2,8 @@
 module Handler.BeatlesLyrics where
 
 import Quiz
-import Handler.Generic (runQuestion,questionWidget)
-import Exception (message,internal)
-import Logic (description)
+import Handler.Generic (genericRoute)
 
-getBeatlesLyricsR seed = do
-  quiz <- getYesod
-  generatedQuestion <- liftIO $ runQuestion seed (beatlesLyrics quiz)
-  case generatedQuestion of
-    (Left ex) -> invalidArgs ["Failed to generate valid question.", message ex, internal ex]
-    (Right question) -> defaultLayout $ do
-      setTitle "Identify the Beatles songs given a snippet of the lyrics"
-      addWidget (questionWidget (BeatlesLyricsR (succ seed)) question)
+getBeatlesLyricsR :: Int -> Handler RepHtml
+getBeatlesLyricsR seed = genericRoute seed beatlesLyrics (BeatlesLyricsR (succ seed))
     
--- bustaquiz/Handler/Capital.hs
+++ bustaquiz/Handler/Capital.hs
@@ -2,16 +2,9 @@
 module Handler.Capital where
 
 import Quiz
-import Handler.Generic (runQuestion,questionWidget)
-import Exception(message,internal)
+import Handler.Generic (genericRoute)
 
 getCapitalsR :: Int -> Handler RepHtml
-getCapitalsR seed = do
-  quiz <- getYesod
-  generatedQuestion <- liftIO $ runQuestion seed (whichCapital quiz)
-  case generatedQuestion of
-    (Left ex) -> invalidArgs ["Failed to generate valid question.", message ex, internal ex]
-    (Right question) -> defaultLayout $ do
-      setTitle "Give the capital cities as quick as you can."
-      addWidget (questionWidget (CapitalsR (succ seed)) question)
+getCapitalsR seed = genericRoute seed whichCapital (CapitalsR (succ seed))
+
 
