a7f8339a5f2d1715229f2df1f1a59d5a2d79f51a
Jeff Foster <jeff.foster@acm.org>
2010-11-17 13:59:13 +0000

    Drastically simplify the rooting.

--- bustaquiz/Handler/Generic.hs
+++ bustaquiz/Handler/Generic.hs
@@ -1,14 +1,15 @@
 {-# LANGUAGE QuasiQuotes, OverloadedStrings, TemplateHaskell #-}
 module Handler.Generic (
   runQuestion,
-  questionWidget) where
+  questionWidget,
+  genericRoute) where
 
 import Control.Exception (try,evaluate)
 import Logic
 import Exception
-import Quiz (Widget,QuizRoute,StaticRoute)
+import Quiz 
 import Settings (cassiusFile,hamletFile,juliusFile,jqueryURL,jqueryUIURL)
-import Yesod.Widget (addCassius,addHamlet,addJulius,addScriptRemote)
+import Yesod.Widget
 import Yesod.Helpers.Static
 import StaticFiles
 import Data.Either
@@ -16,8 +17,19 @@ import Data.Either
 runQuestion :: QuestionMaker a => Int -> a -> IO (Either QuizException Question)
 runQuestion seed qm = try (evaluate =<< generateQuestion seed qm)
                                              
+-- TODO - Should I try to set the title here?
+genericRoute :: QuestionMaker a => Int -> (Quiz -> a) -> QuizRoute -> Handler RepHtml
+genericRoute seed quizFunc next = do
+  quiz <- getYesod
+  generatedQuestion <- liftIO $ runQuestion seed (quizFunc quiz) 
+  case generatedQuestion of
+    (Left ex) -> invalidArgs ["Failed to generate valid question.", message ex, internal ex]
+    (Right question) -> defaultLayout $ do
+      addWidget (questionWidget next question) 
+
+
 -- Display the question as a widget
-questionWidget :: QuizRoute -> Question -> Widget ()
+-- questionWidget :: QuizRoute -> Question -> Widget ()
 questionWidget route (Question description (Associate pairs)) = do
   -- External requirements
   addScriptRemote jqueryURL
--- bustaquiz/Handler/USPresidentsOrder.hs
+++ bustaquiz/Handler/USPresidentsOrder.hs
@@ -2,17 +2,9 @@
 module Handler.USPresidentsOrder where
 
 import Quiz
-import Handler.Generic (runQuestion,questionWidget)
-import Exception(message,internal)
+import Handler.Generic (genericRoute)
 
 getUSPresidentsOrderR :: Int -> Handler RepHtml
-getUSPresidentsOrderR seed = do
-  quiz <- getYesod
-  generatedQuestion <- liftIO $ runQuestion seed (presidentOrder quiz)
-  case generatedQuestion of
-    (Left ex) -> invalidArgs ["Failed to generate valid question.", message ex, internal ex]
-    (Right question) -> defaultLayout $ do
-      setTitle "Put the following US Presidents in order of inauguration."
-      addWidget (questionWidget (CapitalsR (succ seed)) question)
+getUSPresidentsOrderR seed = genericRoute seed presidentOrder (USPresidentsOrderR (succ seed))
 
 
