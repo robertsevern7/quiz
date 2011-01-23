68a343dfaee0f6a2e7e188c2a4dc9de8fead47c9
Jeff Foster <jeff.foster@acm.org>
2011-01-23 18:27:23 +0000

    make sure we go random instead of incrementing

--- bustaquiz/Handler/Taglines.hs
+++ bustaquiz/Handler/Taglines.hs
@@ -4,7 +4,10 @@ module Handler.Taglines where
 import Quiz
 import Logic (QuestionType)
 import Handler.Generic (genericRoute)
+import System.Random
 
 getTaglinesR :: Int -> QuestionType ->  Handler RepHtml
-getTaglinesR seed questionType = genericRoute seed questionType filmTaglines (TaglinesR (succ seed) questionType)
+getTaglinesR seed questionType = do
+  next <- liftIO $ getStdRandom (randomR (1,10000000))
+  genericRoute seed questionType filmTaglines (TaglinesR next questionType)
 
