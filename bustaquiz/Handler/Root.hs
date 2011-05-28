7dab323141881d50a2822d1e936bce6ff1925e52
Jeff Foster <jeff.foster@acm.org>
2011-05-28 17:56:41 +0100

    slightly simplify the root stuff

--- bustaquiz/Handler/Root.hs
+++ bustaquiz/Handler/Root.hs
@@ -14,65 +14,55 @@ import System.Random
 -- functions. You can spread them across multiple files if you are so
 -- inclined, or create a single monolithic file.
 
--- TODO 
--- 1) Get rid of index <- liftIO getIndex
--- 2) Get rid of Bustaquiz
+-- TODO Remove boilerplate crap!
 
 getIndex :: IO Int
 getIndex = getStdRandom (randomR (1,2000000))
 
 getRootR :: Handler RepHtml
-getRootR = do
-    defaultLayout $ do
-        index <- liftIO getIndex
-        setTitle "bustaquiz homepage"
-        addCassius $(cassiusFile "homepage")
-        addWidget $(hamletFile "homepage")
+getRootR = defaultLayout $ do
+  index <- liftIO getIndex
+  setTitle "bustaquiz homepage"
+  addCassius $(cassiusFile "homepage")
+  addWidget $(hamletFile "homepage")
 
 getAcknowledgementsR :: Handler RepHtml
-getAcknowledgementsR = do
-  defaultLayout $ do
-    setTitle "bustaquiz Acknowledgements"
-    addWidget $(hamletFile "acknowledgements")
+getAcknowledgementsR = defaultLayout $ do
+  setTitle "bustaquiz Acknowledgements"
+  addWidget $(hamletFile "acknowledgements")
 
 getWordplayR :: Handler RepHtml
-getWordplayR = do
-    defaultLayout $ do
-        index <- liftIO getIndex
-        addCassius $(cassiusFile "homepage")
-        addWidget $(hamletFile "wordplay")
+getWordplayR = defaultLayout $ do
+  index <- liftIO getIndex
+  addCassius $(cassiusFile "homepage")
+  addWidget $(hamletFile "wordplay")
         
 getGeographyR :: Handler RepHtml
-getGeographyR = do
-    defaultLayout $ do
-        index <- liftIO getIndex
-        addCassius $(cassiusFile "homepage")
-        addWidget $(hamletFile "geography")
+getGeographyR = defaultLayout $ do
+  index <- liftIO getIndex
+  addCassius $(cassiusFile "homepage")
+  addWidget $(hamletFile "geography")
         
 getMusicR :: Handler RepHtml
-getMusicR = do
-    defaultLayout $ do
-        index <- liftIO getIndex
-        addCassius $(cassiusFile "homepage")
-        addWidget $(hamletFile "music")
+getMusicR = defaultLayout $ do
+  index <- liftIO getIndex
+  addCassius $(cassiusFile "homepage")
+  addWidget $(hamletFile "music")
 
 getFilmR :: Handler RepHtml
-getFilmR = do
-    defaultLayout $ do
-        index <- liftIO getIndex
-        addCassius $(cassiusFile "homepage")
-        addWidget $(hamletFile "film")
+getFilmR = defaultLayout $ do
+  index <- liftIO getIndex
+  addCassius $(cassiusFile "homepage")
+  addWidget $(hamletFile "film")
 
 getHistoryR :: Handler RepHtml
-getHistoryR = do
-    defaultLayout $ do      
-        index <- liftIO getIndex
-        addCassius $(cassiusFile "homepage")
-        addWidget $(hamletFile "history")
+getHistoryR = defaultLayout $ do      
+  index <- liftIO getIndex
+  addCassius $(cassiusFile "homepage")
+  addWidget $(hamletFile "history")
         
 getPubQuizR :: Handler RepHtml
-getPubQuizR = do
-    defaultLayout $ do      
-        index <- liftIO getIndex
-        addCassius $(cassiusFile "homepage")
-        addWidget $(hamletFile "pubquiz")
+getPubQuizR = defaultLayout $ do      
+  index <- liftIO getIndex
+  addCassius $(cassiusFile "homepage")
+  addWidget $(hamletFile "pubquiz")
