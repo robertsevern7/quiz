8ade3f3e3d04198356fbc60e51733ab1fc88ec04
Jeff Foster <jeff.foster@acm.org>
2011-05-28 18:04:49 +0100

    specific imports, update comments

--- bustaquiz/Handler/Root.hs
+++ bustaquiz/Handler/Root.hs
@@ -3,18 +3,10 @@ module Handler.Root where
 
 import Quiz
 import Logic
+import System.Random (getStdRandom, randomR)
 
-import System.Random
-
--- This is a handler function for the GET request method on the RootR
--- resource pattern. All of your resource patterns are defined in
--- Quiz.hs; look for the line beginning with mkYesodData.
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
-
--- TODO Remove boilerplate crap!
+-- TODO Remove boilerplate crap!  Slightly difficult due to GHC stage restriction
+-- and template Haskell
 
 getIndex :: IO Int
 getIndex = getStdRandom (randomR (1,2000000))
