e863f6d5a64ca2b5f8f2766f1130767ff5ea26bd
Jeff Foster <jeff.foster@acm.org>
2011-05-30 13:32:31 +0100

    whitespace

--- bustaquiz/Handler/Root.hs
+++ bustaquiz/Handler/Root.hs
@@ -14,11 +14,11 @@ getIndex = getStdRandom (randomR (1,2000000))
 getRootR :: Handler RepHtml
 getRootR = defaultLayout $ do
   index <- liftIO getIndex
-        let quizmenu = $(hamletFile "quizMenu")
+  let quizmenu = $(hamletFile "quizMenu")
   setTitle "bustaquiz homepage"
   addCassius $(cassiusFile "homepage")
   addWidget $(hamletFile "homepage")
-        addJulius $(juliusFile "quizMenu")
+  addJulius $(juliusFile "quizMenu")
 
 getAcknowledgementsR :: Handler RepHtml
 getAcknowledgementsR = defaultLayout $ do
@@ -33,40 +33,40 @@ getWordplayR = defaultLayout $ do
         
 getGeographyR :: Handler RepHtml
 getGeographyR = defaultLayout $ do
-        let quizmenu = $(hamletFile "quizMenu")
+  let quizmenu = $(hamletFile "quizMenu")
   index <- liftIO getIndex
   addCassius $(cassiusFile "homepage")
   addWidget $(hamletFile "geography")
-        addJulius $(juliusFile "quizMenu")
+  addJulius $(juliusFile "quizMenu")
         
 getMusicR :: Handler RepHtml
 getMusicR = defaultLayout $ do
-        let quizmenu = $(hamletFile "quizMenu")
+  let quizmenu = $(hamletFile "quizMenu")
   index <- liftIO getIndex
   addCassius $(cassiusFile "homepage")
   addWidget $(hamletFile "music")
-        addJulius $(juliusFile "quizMenu")
+  addJulius $(juliusFile "quizMenu")
 
 getFilmR :: Handler RepHtml
 getFilmR = defaultLayout $ do
-        let quizmenu = $(hamletFile "quizMenu")
+  let quizmenu = $(hamletFile "quizMenu")
   index <- liftIO getIndex
   addCassius $(cassiusFile "homepage")
   addWidget $(hamletFile "film")
-        addJulius $(juliusFile "quizMenu")
+  addJulius $(juliusFile "quizMenu")
 
 getHistoryR :: Handler RepHtml
 getHistoryR = defaultLayout $ do      
-        let quizmenu = $(hamletFile "quizMenu")
+  let quizmenu = $(hamletFile "quizMenu")
   index <- liftIO getIndex
   addCassius $(cassiusFile "homepage")
   addWidget $(hamletFile "history")
-        addJulius $(juliusFile "quizMenu")
-        
+  addJulius $(juliusFile "quizMenu")
+  
 getPubQuizR :: Handler RepHtml
 getPubQuizR = defaultLayout $ do      
-        let quizmenu = $(hamletFile "quizMenu")
+  let quizmenu = $(hamletFile "quizMenu")
   index <- liftIO getIndex
   addCassius $(cassiusFile "homepage")
   addWidget $(hamletFile "pubquiz")
-        addJulius $(juliusFile "quizMenu")
+  addJulius $(juliusFile "quizMenu")
