f60262a448e5f62446d6925a30cee8fceeb08c33
Jeff Foster <jeff.foster@acm.org>
2011-05-31 08:19:09 +0100

    remove commented out code, so we can use version control instead

--- bustaquiz/Freebase.hs
+++ bustaquiz/Freebase.hs
@@ -57,16 +57,3 @@ directorQuery = Mapping [
   ("type", Scalar $ toJsonScalar "/film/film"),
   ("directed_by", Mapping [("id",Scalar $ toJsonScalar "/en/peter_jackson")])
   ]
-
--- runSimpleQuery :: String -> String -> String -> JsonScalar -> (JsonScalar -> String) -> IO (String, Result [String])
--- runSimpleQuery qtype key id_ arr extractor = do
---   response <- runQuery $ mkSimpleQuery [("type",toJsonScalar qtype),("id",toJsonScalar id_), ("name", JsonNull),(key,arr)]
---   return response
---   {-
---   let k = lookupValue response "result"
---       (Ok name) = lookupValue (getFirst k) "name"
---       l = lookupValue (getFirst k) key
---       m = fmap (\(JSArray x) -> x) l
---   return (getString name, fmap (map extractor) m)
-
-
